adfffa5112970207a5c794f77da32bc59c095bb5
Flaze <18499438+FlazeNaive@users.noreply.github.com>
2024-03-28 14:06:28 +0100

    add json api

--- HW1/data_structures.py
+++ HW1/data_structures.py
@@ -1,15 +1,75 @@
-# data_structures.py
-def initialize_processor():
-    return {
-        "PC": 0,
-        "PhysicalRegisterFile": [0] * 64,
-        "DecodedInstructions": [],
-        "ExceptionFlag": False,
-        "ExceptionPC": 0,
-        "RegisterMapTable": list(range(32)),
-        "FreeList": list(range(32, 64)),
-        "BusyBitTable": [False] * 64,
-        "ActiveList": [],
-        "IntegerQueue": [],
-        "halt": False  # 控制模拟器是否停止
-    }
+class ActiveListEntry:
+    def __init__(self, LogDes=0, OldDes=0, PC=0):
+        self.Done = False
+        self.Exception = False
+        self.LogDes = LogDes
+        self.OldDes = OldDes
+        self.PC = PC
+    def __repr__(self):
+        return ("ActiveListEntry(Done=" + str(self.Done) + ", " 
+                "Exception=" + str(self.Exception) + ", "
+                "LogicalDestination=" + str(self.LogicalDestination) + ", "
+                "OldDestination=" + str(self.OldDestination) + ", "
+                "PC=" + str(self.PC) + ")")
+
+class IntegerQueueEntry:
+    def __init__(self, DestRegister=0, 
+                        OpAIsReady=False, 
+                        OpARegTag=-1, 
+                        OpAValue=0.0, 
+                        OpBIsReady=False, 
+                        OpBRegTag=-1, 
+                        OpBValue=0.0, 
+                        OpCode="NOP", 
+                        PC=0):
+        self.DestRegister = DestRegister
+        self.OpAIsReady = OpAIsReady
+        self.OpARegTag = OpARegTag
+        self.OpAValue = OpAValue
+        self.OpBIsReady = OpBIsReady
+        self.OpBRegTag = OpBRegTag
+        self.OpBValue = OpBValue
+        self.OpCode = OpCode
+        self.PC = PC
+    
+    def __repr__(self):
+        return ("IntegerQueueEntry(DestRegister=" + str(self.DestRegister) + ", " 
+                "OpAIsReady=" + str(self.OpAIsReady) + ", "
+                "OpARegTag=" + str(self.OpARegTag) + ", "
+                "OpAValue=" + str(self.OpAValue) + ", "
+                "OpBIsReady=" + str(self.OpBIsReady) + ", "
+                "OpBRegTag=" + str(self.OpBRegTag) + ", "
+                "OpBValue=" + str(self.OpBValue) + ", "
+                "OpCode='" + self.OpCode + "', "
+                "PC=" + str(self.PC) + ")")
+
+                
+                
+                
+def compare_json(obj1, obj2, path=""):
+    """递归比较两个JSON对象，并打印不同之处。"""
+    # print("isdict ojb1: ", isinstance(obj1, dict))
+    # print("isdict ojb2: ", isinstance(obj2, dict))
+    # print("islist ojb1: ", isinstance(obj1, list))
+    # print("islist ojb2: ", isinstance(obj2, list))
+
+    if isinstance(obj1, dict) and isinstance(obj2, dict):
+        for key in obj1:
+            if key not in obj2:
+                print(f"{path}.{key} 在第二个JSON中不存在")
+            else:
+                compare_json(obj1[key], obj2[key], f"{path}.{key}")
+        for key in obj2:
+            if key not in obj1:
+                print(f"{path}.{key} 在第一个JSON中不存在")
+    elif isinstance(obj1, list) and isinstance(obj2, list):
+        for i, item in enumerate(obj1):
+            if i >= len(obj2):
+                print(f"{path}[{i}] 在第二个JSON中不存在")
+            else:
+                compare_json(item, obj2[i], f"{path}[{i}]")
+        for i in range(len(obj1), len(obj2)):
+            print(f"{path}[{i}] 在第一个JSON中不存在")
+    else:
+        if obj1 != obj2:
+            print(f"{path} 不同: {obj1} != {obj2}")
--- HW1/simulator.py
+++ HW1/simulator.py
@@ -1,5 +1,5 @@
 # simulator.py
-from data_structures import initialize_processor
+from data_structures import ActiveListEntry, IntegerQueueEntry, compare_json
 from instruction import decode_instruction
 
 import json
@@ -7,34 +7,52 @@ import json
 class Simulator:
     def __init__(self):
         self.instructions = []
+        self.parsedInstructions = []
         self.processor_state = {
             "PC": 0,
             "PhysicalRegisterFile": [0] * 64,
-            "DecodedInstructions": [],
-            "ExceptionFlag": False,
+            "DecodedPCs": [],
             "ExceptionPC": 0,
+            "Exception": False,
             "RegisterMapTable": list(range(32)),
             "FreeList": list(range(32, 64)),
             "BusyBitTable": [False] * 64,
             "ActiveList": [],
-            "IntegerQueue": [],
-            "halt": False  # 控制模拟器是否停止
+            "IntegerQueue": []
+            #, "halt": False  # 控制模拟器是否停止
         }
 
     def load_instructions(self, input_file_path):
         # 读取指令集文件并存储
         # load instructions from json file
         self.instructions = json.load(open(input_file_path))
+        for ins in self.instructions:
+            opcode, operands = decode_instruction(ins)
+            self.parsedInstructions.append((opcode, operands))
+
+        # Only for test
+        print("[INFO] Instructions loaded")
 
 
     def run(self):
-        # Only for test
-        for ins in self.instructions:
-            opcode, operands = decode_instruction(ins)
-            print(opcode, operands)
-            self.processor_state["DecodedInstructions"].append((opcode, operands))
-        print("[INFO] Instructions loaded")
+        pass
+
 
     def print_state(self):
         # 输出处理器的当前状态
-        print(self.processor_state)
+        # print state as json
+        # to print: ActiveList, BusybitTable, DecodedPCs, 
+        #           Exception, ExceptionPC, 
+        #           FreeList, 
+        #           IntegerQueue, PC,
+        #           PhysicalRegisterFile
+        #           RegisterMapTable
+        print(json.dumps(self.processor_state, indent=4))
+    
+    def debug_check_same(self, std_state):
+        mystate = json.loads(json.dumps(self.processor_state))
+        # check if two json are the same
+        if mystate == std_state:
+            print("两个JSON对象相同")
+        else:
+            compare_json(mystate, std_state)
